bdcefe55641d3619fadf653de4c708a7ddc925fd
Lany Evora monteiro <lanyevora@gmail.com>
2023-11-30 19:47:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace slotMachine_1Attempt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] simbols = { 'A', 'B', 'C', 'D', 'E' };
+            
+            Console.WriteLine("Welcome to the Slot Machine Game!");
+            
+
+        }
+    }
+}
